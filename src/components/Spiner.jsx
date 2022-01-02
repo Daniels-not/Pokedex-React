506fc454a89f6b6c53ac9bdff8cd8fef65867e56
Daniels-not <ramydcampusano@hotmail.com>
2022-01-02 02:25:09 -0400

    loader added

--- src/components/Spiner.jsx
+++ src/components/Spiner.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Classes from './Spiner.module.css'
 
 const Spiner = () => {
     return (
-        <div className="spiner">
-            <div className="spiner__loader"></div>
+        <div className={Classes.spiner_container}>
+            <div className={Classes.spiner__loader}></div>
         </div>
     )
 }
